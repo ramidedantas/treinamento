b0c71afffd43abda787bf29b47c0af581cd0324b
Ramide Dantas <ramidedantas@recife.ifpe.edu.br>
2025-05-23 11:01:37 -0300

    BC2596 - Xenlonguinho - Atualização

--- BC2596 - Xenlonguinho/main.cpp
+++ BC2596 - Xenlonguinho/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <valarray>
+
 #define MAX 1000
 
 using namespace std;
@@ -14,24 +16,43 @@ void init(vector<int> &divs) {
     }
 }
 
+void init2(vector<int> &even_divs) {
+    vector<bool> has_even_divs(MAX + 1, true);
+
+    int lim = (int)sqrt(MAX);
+    for (int v = 1; v <= lim; v++) {
+        has_even_divs[v*v] = false;
+    }
+
+    int even_div = 0;
+    for (int i = 1; i <= MAX; i++) {
+        if (has_even_divs[i]) even_div++;
+        even_divs[i] = even_div;
+    }
+}
+
 int main() {
     int c;
     cin >> c;
 
-    vector<int> divs(MAX + 1, 0);
+//    vector<int> divs(MAX + 1, 0);
+//    init(divs); // O(K)
 
-    init(divs); // O(K)
+    vector<int> even_divs(MAX + 1, 0);
+    init2(even_divs);
 
     while (c--) {
         int n;
         cin >> n;
 
-        int impares = 0;
-        for (int i = 1; i <= n; i++) { // O(n)
-            if (divs[i] % 2) impares++;
-        }
+//        int impares = 0;
+//        for (int i = 1; i <= n; i++) { // O(n)
+//            if (divs[i] % 2) impares++;
+//        }
+//
+//        cout << n - impares << endl;
 
-        cout << n - impares << endl;
+        cout << even_divs[n] << endl;
     }
     return 0;
 }
