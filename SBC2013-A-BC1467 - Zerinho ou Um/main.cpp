cf64b156ac0b5784e59ef20cb2e7958043d262a1
Ramide Dantas <ramidedantas@recife.ifpe.edu.br>
2025-08-07 19:33:37 -0300

    SBC2013-A-BC1467 - Zerinho ou Um

---
+++ SBC2013-A-BC1467 - Zerinho ou Um/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+//    string res = "*CBAABC*";
+    int a, b, c;
+
+    while(cin >> a >> b >> c)
+//        int caso = 4 * a + 2 * b + c;
+        cout << "*CBAABC*"[4 * a + 2 * b + c] << endl;
+
+
+    return 0;
+}
