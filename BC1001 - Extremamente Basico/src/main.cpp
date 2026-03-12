daf66ff8be451fc47dee2731d712cdd5f0c58acb
Ramide Dantas <ramidedantas@recife.ifpe.edu.br>
2026-03-12 16:31:33 -0300

    BC1001 - Extremamente Basico

---
+++ BC1001 - Extremamente Basico/src/main.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : BC1001.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+
+	cin >> a >> b;
+
+	cout << "X = " << (a + b) << endl;
+
+	return 0;
+}
